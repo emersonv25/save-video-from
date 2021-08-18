34766d0d84d0481d9d26350c6c221e56ce939a77
emersonv25 <emersondejesussantos@hotmail.com>
2021-08-18 17:55:58 -0300

    update

--- api/src/controllers/controller.js
+++ api/src/controllers/controller.js
@@ -1,5 +1,4 @@
 const youtubedl = require('youtube-dl-exec')
-//const ytdl = require('ytdl-core');
 
 exports.getUrl = async (req, res) => {
   if(req.query.url){
@@ -30,35 +29,3 @@ exports.getInfo = (req, res) => {
     }).then(output => res.status(200).send(output)).catch(err => res.status(400).send(err))
   }
 }
-/*
-exports.download2 = (req, res) => {
-  var url = req.query.url;    
-  res.header("Content-Disposition", 'attachment;\  filename="Video.mp4');    
-  ytdl(url, {format: 'mp4'}).pipe(res);
-}
-*/
-
-
-
-//https://github.com/ytdl-org/youtube-dl/blob/master/README.md
-//https://github.com/microlinkhq/youtube-dl-exec
-
-/*
-exports.post = (req, res, next) => {
-  res.status(201).send('Rota POST!');
-};
-
-exports.put = (req, res, next) => {
-  let id = req.params.id;
-  res.status(201).send(`Rota PUT com ID! --> ${id}`);
-};
-
-exports.delete = (req, res, next) => {
-  let id = req.params.id;
-  res.status(200).send(`Rota DELETE com ID! --> ${id}`);
-};
-
-exports.getById = (req, res, next) => {
-  let id = req.params.id;
-  res.status(200).send(`Rota GET com ID! ${id}`);
-};*/
