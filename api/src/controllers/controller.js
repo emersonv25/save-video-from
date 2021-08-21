a9492ce5af059455d401b0020e908e3a80d93d0a
emersonv25 <emersondejesussantos@hotmail.com>
2021-08-20 21:35:45 -0300

    menos if

--- api/src/controllers/controller.js
+++ api/src/controllers/controller.js
@@ -1,31 +1,20 @@
 const youtubedl = require('youtube-dl-exec')
 
+// quality = worst, best, bestaudio
 exports.getUrl = async (req, res) => {
-  if(req.query.url){
-    if(req.query.quality == 'hd'){
-      youtubedl(req.query.url, {
-        format: 'best[protocol^=http]',
-        getUrl: true,
-      }).then(output => res.status(200).send(output)).catch(err => res.status(400).send(err))
-    }
-    if(req.query.quality == 'sd'){
-      youtubedl(req.query.url, {
-        format: 'worst[protocol^=http]',
-        getUrl: true,
-      }).then(output => res.status(200).send(output)).catch(err => res.status(400).send(err))
-    }
-    if(req.query.quality == 'audio'){
-      youtubedl(req.query.url, {
-        format: 'bestaudio[protocol^=http]',
-        getUrl: true,
-      }).then(output => res.status(200).send(output)).catch(err => res.status(400).send(err))
-    }
+  if(req.query.url && req.query.quality){
+    youtubedl(req.query.url, {
+      format: req.query.quality + '[protocol^=http]',
+      getUrl: true,
+    }).then(output => res.status(200).send(output)).catch(err => res.status(400).send(err))
+  }
+  else{
+    res.status(400).send("PARAMETROS INVALIDOS")
   }
 };
 exports.getInfo = (req, res) => {
   if(req.query.url){
-    youtubedl(req.query.url, {
-      dumpJson: true,
-    }).then(output => res.status(200).send(output)).catch(err => res.status(400).send(err))
-  }
-}
+  youtubedl(req.query.url, {
+    dumpJson: true,
+  }).then(output => res.status(200).send(output)).catch(err => res.status(400).send(err))}
+};
